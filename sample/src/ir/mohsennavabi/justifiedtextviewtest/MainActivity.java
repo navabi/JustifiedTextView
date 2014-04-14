c07de2f5794a4045219a7be9b428d16d0c7751fa
navabi <navabi70@gmail.com>
2014-04-14 16:12:01 +0430

    add Sample Project

---
+++ sample/src/ir/mohsennavabi/justifiedtextviewtest/MainActivity.java
@@ -0,0 +1,16 @@
+package ir.mohsennavabi.justifiedtextviewtest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
